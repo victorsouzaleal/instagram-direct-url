4b1bb0874a2f69a5255537f486b1fac5ae3e53ef
victorsouzaleal <victorsouzaleal@gmail.com>
2021-03-06 14:33:19 -0300

    fixed download

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const axios = require("axios"), cheerio = require("cheerio"),qs = require('qs')
 module.exports = instagramGetUrl = (url_media) =>{
     return new Promise((resolve,reject)=>{
-        var url = "https://sssinstagram.com/results"
+        url_media = url_media.replace("reel", "p")
+        var url = url_media.replace("instagram","sssinstagram")
         const requestBody = {
             id: url_media.replace("reel", "p"),
+            tt: "69798d4e7d1623a164eb3930e17246c8",
+            ts: 1615050939
         }
         const config = {
             headers: {
@@ -12,10 +15,17 @@ module.exports = instagramGetUrl = (url_media) =>{
         }
         axios.post(url, qs.stringify(requestBody), config).then(result => {
             let $ = cheerio.load(result.data), ig = []
-            $('div.button_div > a').each((i, element) => {
+            $('div.result_overlay > div > div > a').each((i, element) => {
                 let cheerioElement = $(element)
                 ig.push(cheerioElement.attr("href"))
             })
+    
+            if(ig.length == 0){
+                $('div.result_overlay > a').each((i, element) => {
+                    let cheerioElement = $(element)
+                    ig.push(cheerioElement.attr("href"))
+                })
+            }
             resolve({
                 results_number : ig.length,
                 url_list: ig

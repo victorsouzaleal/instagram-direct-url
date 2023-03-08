58e957dffc11e893443e1b20b29e05c7973c9f05
Autumn <93840371+autumn1111@users.noreply.github.com>
2023-03-08 14:28:25 +0530

    feat: Refactor instagramGetUrl function
    . This pull request refactors the `instagramGetUrl` function to improve code clarity, readability, and maintainability. 
    . The changes include:
    
    - Updated the function to return scontent.cdninstagram.com links again instead of media.instasupersave.com links as shown in README.md.
    - Updated function signature to include JSDoc comments describing the function and its parameters.
    - Wrapped the function in an `async` block to allow for the use of `await`.
    - Updated the try/catch block to use `await` instead of the `.then()` method.
    - Refactored the `config ` variable to `requestHeaders` to better reflect its content.
    - Make use of `const` instead of `var`.
    - Updated the names of the input parameter and return values to reflect their use.
    - Updated the `reject` return value to include the error message instead of just the message.
    - Added a return value for when the API call fails to provide more information about the error.
    
    . With contributions from: autumn1111 <autumnwtf@proton.me>

--- src/index.js
+++ src/index.js
@@ -1,53 +1,51 @@
-const axios = require("axios")
-module.exports = instagramGetUrl = (url_media) =>{
-    return new Promise(async (resolve,reject)=>{
-        const BASE_URL = "https://instasupersave.com/"
-        
-        //New Session = Cookies
-        try {
-            const resp = await axios(BASE_URL);
-            const cookie = resp.headers["set-cookie"]; // get cookie from request
-            const session = cookie[0].split(";")[0].replace("XSRF-TOKEN=","").replace("%3D", "")
-            
-            //REQUEST CONFIG
-            var config = {
-                method: 'post',
-                url: `${BASE_URL}api/convert`,
-                headers: { 
-                    'origin': 'https://instasupersave.com', 
-                    'referer': 'https://instasupersave.com/pt/', 
-                    'sec-fetch-dest': 'empty', 
-                    'sec-fetch-mode': 'cors', 
-                    'sec-fetch-site': 'same-origin', 
-                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52', 
-                    'x-xsrf-token': session, 
-                    'Content-Type': 'application/json', 
-                    'Cookie': `XSRF-TOKEN=${session}; instasupersave_session=${session}`
-                },
-                data : {
-                    url: url_media
-                }
-            };
+const axios = require('axios');
 
-            //REQUEST
-            axios(config).then(function (response) {
-                let ig = []
-                if(Array.isArray(response.data)){
-                    response.data.forEach(post => { ig.push(post.sd === undefined ? post.thumb : post.sd.url)})
-                } else {
-                    ig.push(response.data.url[0].url)    
-                }
-                
-                resolve({
-                    results_number : ig.length,
-                    url_list: ig
-                })
-            })
-            .catch(function (error) {
-                reject(error.message)
-            })
-        } catch(e){
-            reject(e.message)
-        }
-    })
+/** 
+ * This function intakes an instagramUrl of an image or video post, reel, or public account stories using InstaSuperSave API.
+ * @param {string} instagramUrl - The Instagram URL to convert.
+ * @returns {Promise<Array<{fileExtension: string, rawUrl: string}> | string>} An array of objects containing fileExtension and rawUrl properties, If the API call fails, returns error message with additional information.
+ */
+async function instagramGetUrl(instagramUrl) {
+    const api = 'https://instasupersave.com';
+	// Collect cookie from session.
+	try {
+		const {
+			headers: { 'set-cookie': cookie }
+		} = await axios(api);
+		const session = String(cookie[0].split(';')[0]).replace('XSRF-TOKEN=', '').replace('%3D', '');
+
+		// Setup request headers.
+		const requestHeaders = {
+			method: 'POST',
+			url: `${api}/api/convert`,
+			headers: {
+				'Origin': api,
+				'Referer': `${api}/pt/`,
+				'Sec-Fetch-Dest': 'empty',
+				'Sec-Fetch-Mode': 'cors',
+				'Sec-Fetch-Site': 'same-origin',
+				'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52',
+				'X-XSRF-Token': session,
+				'Content-Type': 'application/json',
+				'Cookie': `XSRF-TOKEN=${session}; instasupersave_session=${session}`
+			},
+			data: { url: instagramUrl }
+		};
+
+		// Call the API and process the data.
+		const { data } = await axios(requestHeaders);
+		return data.map(({ url }) => {
+			const { ext: fileExtension, url: rawUrl } = url[0];
+			return { fileExtension, rawUrl };
+		});
+	} catch (error) {
+		return (await import('util')).format(
+			'[instagram-direct-url] %s (Status: %s, Status Text: %s)', 
+			error?.message || 'An error occurred.', 
+			error?.response?.status || 'None', 
+			error?.response?.statusText || 'None'
+		);
+	}
 }
+
+module.exports = instagramGetUrl;

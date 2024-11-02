815d6c247f63ea16dae3bd804afe09a323bac606
victorsouzaleal <victorsouzaleal@gmail.com>
2024-11-02 08:33:24 -0300

    api changed again...

--- src/index.js
+++ src/index.js
@@ -6,33 +6,33 @@ axios.defaults.timeout = 0
 module.exports = instagramGetUrl = (url_media) =>{
     return new Promise(async (resolve,reject)=>{
         try {
-            const BASE_URL = "https://aiovd.com/wp-json/aio-dl/video-data/"
-
-            const dataBody = qs.stringify({
-                'url': url_media,
-                'token': '0d1a286f793e99721ae2b508c53abf74cc88d27e6ad7daedcaee8ef0ae749b8c' 
-            });
-              
+            const BASE_URL = "https://gramsaver.com/api/"
+            const split_url_media = url_media.split("/")
+            const index_id_video = split_url_media.findIndex(item => item == "p" || item == "reel") + 1
+            const ID_VIDEO = split_url_media[index_id_video]
             const config = {
-                method: 'post',
-                maxBodyLength: Infinity,
-                url: BASE_URL,
-                headers: { 
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                data : dataBody
+                method: 'get',
+                url: BASE_URL+ID_VIDEO,
             };
-              
             const {data} = await axios.request(config)
-
             if (!data) reject({results_number: 0 , url_list: []})
-            
             let url_list = []
-
-            data.medias.forEach(media => {
-                url_list.push(media.url)
-            })
-            
+            if(data.media){
+                data.media.forEach(media => {
+                    console.log(media)
+                    if(media.node.is_video){
+                        url_list.push(media.node.video_url)
+                    } else {
+                        url_list.push(media.node.display_url)
+                    }
+                })
+            } else {
+                if (data.is_video){
+                    url_list.push(data.video_url)
+                } else {
+                    url_list.push(data.photo_url)
+                }
+            }
             resolve({results_number: url_list.length , url_list})
         } catch(err){
             reject(err)
